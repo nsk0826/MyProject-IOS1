e1967b18a10471fafb2c5ac000edd25d4a6edf66
nsk0826 <nsk.20160401@gmail.com>
2021-12-21 00:49:33 +0900

    Initial Commit

---
+++ MyProject1/MyProject1App.swift
@@ -0,0 +1,17 @@
+//
+//  MyProject1App.swift
+//  MyProject1
+//
+//  Created by ota shunsuke on 2021/12/21.
+//
+
+import SwiftUI
+
+@main
+struct MyProject1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
